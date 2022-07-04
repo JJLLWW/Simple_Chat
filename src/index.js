dd9349405d2327a024cedafcae919026102f5819
Jack Wright <jack.louie.wright@gmail.com>
2022-07-04 20:15:14 +0100

    client semi-usable

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import io from 'socket.io-client';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import {
+  ThemeProvider,
+  createTheme,
+} from '@mui/material/styles';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const DarkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+// stupid way to share the socket on browser
+const socket = io('ws://localhost:3000');
+socket.emit('get_msg');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={DarkTheme}>
+      <CssBaseline />
+      <App sock={socket} />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
